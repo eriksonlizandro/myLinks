7d865b62b4599e56c3e88ef1ad466254f9a06ccb
eriksonlizandro <eegs.sist@gmail.com>
2021-05-20 19:52:18 -0600

    load-background bug test2

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,24 @@
 import * as React from "react"
 import App from '../components/App'
+import { createGlobalStyle } from "styled-components"
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    height: 100%;
+    background-repeat: no-repeat;
+    background-attachment: fixed;
+    background: linear-gradient(to left, #ddd6f3 0%, #faaca8 100%);
+    display:block;
+  
+  }
+`
 
 const IndexPage = () => {
   return(
+    <div>
+    <GlobalStyle/>
     <App/>
+    </div>
   )
 }
 
